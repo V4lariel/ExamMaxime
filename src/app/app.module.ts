ce7b33314e3e6186bbdf5916df7f44b55b18c8b3
V4lariel <erevan.vlr@gmail.com>
2020-02-20 09:39:05 +0100

    creation components, models, services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { MenuComponent } from './components/menu/menu.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddComputerComponent } from './components/add-computer/add-computer.component';
+import { EditComputerComponent } from './components/edit-computer/edit-computer.component';
+import { ComputerDetailsComponent } from './components/computer-details/computer-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DashboardComponent,
+    AddComputerComponent,
+    EditComputerComponent,
+    ComputerDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/computer-details/computer-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-computer-details',
+  templateUrl: './computer-details.component.html',
+  styleUrls: ['./computer-details.component.css']
+})
+export class ComputerDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
